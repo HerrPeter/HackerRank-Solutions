da7b4464c8e27b062d92133d046eae9e5ecce161
Peter Kaufinger <peterkaufinger@gmail.com>
2019-01-08 21:43:45 -0800

    Finished Jumping On Clouds challenge.

--- Interview Prep Kit/Jumping on the Clouds/main.js
+++ Interview Prep Kit/Jumping on the Clouds/main.js
@@ -1,7 +1,32 @@
 'use strict';
 
-function main() {
+/**
+ * Complete the jumpingOnClouds function below.
+ * @param {array} c - The array of binary integers representing good and bad clouds.
+ */
+function jumpingOnClouds(c) {
+    let currIndex = 0,
+        jumps = 0;
+    while (currIndex < c.length - 1) {
+        if (c[currIndex + 2] == 0) {
+            currIndex += 2;
+        } else if (c[currIndex + 1] == 0) {
+            currIndex += 1;
+        } else {
+            // May need to search through rest of binary-integers for a 0.
+            currIndex += 1;
+        }
 
+        jumps++;
+    }
+
+    return jumps;
+}
+
+// The entry point of the program.
+function main() {
+    let clouds = [0, 0, 1, 0, 0, 1, 0];
+    console.log(jumpingOnClouds(clouds));
 }
 
 // Start of the program.
