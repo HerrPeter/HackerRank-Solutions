36c0fbce1bd58592a5fc9ab7a8af981701c72375
Peter Kaufinger <peterkaufinger@gmail.com>
2019-01-10 03:45:58 -0800

    Added sock merchant challenge to the interview prep kit.

--- Interview Prep Kit/Sock Merchant/main.js
+++ Interview Prep Kit/Sock Merchant/main.js
@@ -1,31 +1,39 @@
 'use strict';
 
 /**
- * 
- * @param {number} sockCount 
- * @param {Array} sockArr 
+ * Return the number of pairs of socks (unique pairs in the array).
+ * @param {number} sockCount - Number of socks in the sockArr.
+ * @param {Array} sockArr - Array of socks (unsorted) represented as integers.
  */
 function sockMerchant(sockCount, sockArr) {
-    let tempArr = sockArr;
+    let tempArr = sockArr,
+        numOfPairs = 0;
 
     // Sort array.
-    sockArr.sort();
+    tempArr.sort();
 
     // Search array.
     // If first two items are the same -> remove them and add one to number of pairs
     // Else Remove first item and compare next two (prev condition)
-
-
-    while (tempArr.length > 0) {
-        while (tempArr.length) {
-
+    while (tempArr.length > 1) {
+        if (tempArr[0] === tempArr[1]) {
+            // Pair of socks found
+            tempArr.shift();
+            tempArr.shift();
+            numOfPairs++;
+        } else {
+            tempArr.shift();
         }
     }
+
+    return numOfPairs;
 }
 
 // The entry point of the program.
 function main() {
-
+    let arr = [22, 10, 2, 22, 2, 13, 13, 10, 2];
+    let num = 7;
+    console.log(sockMerchant(num, arr));
 }
 
 // The start of the program.
