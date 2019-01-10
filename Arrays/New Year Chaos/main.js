ff8a1b7f0d894507abcb199da08663146baf86c1
Peter Kaufinger <peterkaufinger@gmail.com>
2019-01-10 04:16:16 -0800

    Prep the New Year Chaos challenge.

---
+++ Arrays/New Year Chaos/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Entry point of the program.
+function main() {
+    let arr = [1, 2, 3, 4, 5];
+    let numOfRot = 2;
+    console.log();
+}
+
+// Start of the program.
+main();
