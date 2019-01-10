790056ec5e3a945ae83773087b18bd38b57fc6f0
Peter Kaufinger <peterkaufinger@gmail.com>
2019-01-10 02:55:09 -0800

    Add main.js

---
+++ Interview Prep Kit/Sock Merchant/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * 
+ * @param {number} sockCount 
+ * @param {Array} sockArr 
+ */
+function sockMerchant(sockCount, sockArr) {
+    let tempArr = sockArr;
+
+    // Sort array.
+    sockArr.sort();
+
+    // Search array.
+    // If first two items are the same -> remove them and add one to number of pairs
+    // Else Remove first item and compare next two (prev condition)
+
+
+    while (tempArr.length > 0) {
+        while (tempArr.length) {
+
+        }
+    }
+}
+
+// The entry point of the program.
+function main() {
+
+}
+
+// The start of the program.
+main();
